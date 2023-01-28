7440e36cd34e0e691951920423538eb49ca865fb
Awal hossain <waqin21@gmail.com>
2023-01-28 21:22:55 +0600

    toolbar import ⚒️

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -2,6 +2,22 @@ import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
 import React, { useCallback } from 'react';
 
+
+
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+]
+
+
+
 export default function TextEditor() {
   // const wrapperRef = useRef();
 
@@ -11,7 +27,10 @@ export default function TextEditor() {
     wrapper.innerHTML = '';
       const editor = document.createElement('div');
       wrapper.append(editor);
-        new Quill(editor, {theme: "snow"})
+        new Quill(editor, {
+          theme: "snow", 
+        modules: {toolbar: TOOLBAR_OPTIONS}
+      })
 
       
     },[])
