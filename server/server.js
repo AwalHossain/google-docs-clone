4cdde64d35c059625579e8707b056cda279302b3
Awal hossain <waqin21@gmail.com>
2023-01-29 22:25:49 +0600

    document join in room 🎉🎉

--- server/server.js
+++ server/server.js
@@ -7,10 +7,16 @@ const io = require('socket.io')(3001,{
 
 
 io.on("connection", (socket)=>{
-
-    socket.on("send-changes", (delta)=>{
-        socket.broadcast.emit("receive-changes", delta);
+    let data = "";
+    socket.on('get-document', documentId =>{
+        socket.join(documentId);
+        socket.emit("load-document", data);
+        socket.on("send-changes", (delta)=>{
+            socket.broadcast.to(documentId).emit("receive-changes", delta);
+        })
     })
+
+
 
     console.log("connected");
 })
