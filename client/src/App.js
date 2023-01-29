e69e2fb70bcb4b1a2366eb667b2a0c6052149f84
Awal hossain <waqin21@gmail.com>
2023-01-29 21:55:22 +0600

    react-router 🛖

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 
+import {
+  BrowserRouter as Router, Redirect, Route, Switch
+} from 'react-router-dom';
+import { v4 as uuidV4 } from 'uuid';
 import './style.css';
 import TextEditor from './TextEditor';
-
 function App() {
   return (
-      <TextEditor />
+     
       // <UseRef />
+      <Router>
+        <Switch>
+        <Route path="/" exact>
+         <Redirect to={`/documents/${uuidV4()}`} />
+        </Route>
+        <Route path="/documents/:id">
+         <TextEditor />
+        </Route>
+        </Switch>
+      </Router>
 
   );
 }
