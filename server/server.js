cb766a26557f0393ac147acbc029299579816f15
Awal hossain <waqin21@gmail.com>
2023-01-30 12:58:10 +0600

    find or create document ✅

--- server/server.js
+++ server/server.js
@@ -21,10 +21,11 @@ const io = require('socket.io')(3001,{
 
 
 io.on("connection", (socket)=>{
-    let data = "";
-    socket.on('get-document', documentId =>{
+    // let data = "";
+    socket.on('get-document', async(documentId) =>{
+        const document = await findOrCreateDocument(documentId);
         socket.join(documentId);
-        socket.emit("load-document", data);
+        socket.emit("load-document", document.data);
         socket.on("send-changes", (delta)=>{
             socket.broadcast.to(documentId).emit("receive-changes", delta);
         })
@@ -34,3 +35,16 @@ io.on("connection", (socket)=>{
 
     console.log("connected");
 })
+
+const defaultValue = "";
+
+async function findOrCreateDocument(id){
+    if(id == null)  return;
+
+    const document = await Document.findById(id);
+
+    if(document) return document;
+
+    return await Document.create({_id: id, data: defaultValue});
+
+}
