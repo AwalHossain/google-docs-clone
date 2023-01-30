daca95eab30320cb21d3e8e8fa06575ce9af320d
Awal hossain <waqin21@gmail.com>
2023-01-30 22:32:25 +0600

    code changes ✅

--- server/server.js
+++ server/server.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+
+const app = require('express')();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const mongoose = require('mongoose');
 const Document = require("./Document");
 
@@ -7,12 +18,15 @@ mongoose.connect("mongodb+srv://documents:BAQTFVLBcI896jbe@cluster0.33slg.mongod
 
 
 
-const io = require('socket.io')(3001,{
-    cors:{
-        origin: "*",
-        methods: ['GET', 'POST']
-    }
+// Socket
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
 });
+
 
 
 io.on("connection", (socket)=>{
@@ -49,3 +63,13 @@ async function findOrCreateDocument(id){
     return await Document.create({_id: id, data: defaultValue});
 
 }
+
+app.get('/', (req, res) => {
+    res.send('hello');
+});
+
+
+
+http.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
